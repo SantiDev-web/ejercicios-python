94804862bbe80d7afc1ce97accfda95373a65629
SantiDev <santiago.luqueto@gmail.com>
2024-06-21 13:21:21 +0200

    Crud Users

--- FastApi/users.py
+++ FastApi/users.py
@@ -26,6 +26,7 @@ async def usersjson():
 
 #
 
+# !Metodo Get
 @app.get("/users")
 async def users():
     return users_list
@@ -41,8 +42,47 @@ async def user(id: int):
 @app.get("/user/")
 async def user(id: int):
     return search_user(id)
-    
 
+# !Metodo Post
+
+@app.post("/user/")
+async def CreateUser(user: User):
+    if type(search_user(user.id)) == User:
+        return {"Error": "El usuario ya existe"}
+    else:
+        users_list.append(user)
+        return user
+
+# !Metodo Put
+
+@app.put("/user/")
+async def updateUser(user: User):
+    found = False
+    
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+    if not found:
+        return {"Error": "El usuario no ha sido actualizado"}
+    else:
+        return user
+
+# !Metodo Delete
+
+@app.delete("/user/{id}")
+async def deleteUser(id: int):
+    found = False
+    
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+            found = True
+    if not found:
+        return {"Error": "El usuario no se ha eliminado"}
+    else:
+        return{"El usuario se ha eliminado con exito"}
+    
 
 def search_user(id: int):
     users = filter(lambda user:user.id == id, users_list)
@@ -50,3 +90,5 @@ def search_user(id: int):
         return list(users)[0]
     except:
         return{"Error": "Usuario no encontrado"}
+    
+
